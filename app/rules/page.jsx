4d338b5a63b69c5556630fad7ceb601280dfea95
Yudetel <yur83798@gmail.com>
2025-09-05 17:59:16 +0300

    auth4

--- app/rules/page.jsx
+++ app/rules/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 
 export default function Home() {
   const [rules, setRules] = useState([]);
