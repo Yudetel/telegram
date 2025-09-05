e03c3ffb21040c4daf34c979cf0a1c73fb0408d7
Yudetel <yur83798@gmail.com>
2025-09-06 02:23:14 +0300

    adaptive10

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="uk">
-      <body>
+      <body style={{ position: "relative" }}>
         <Providers>
           <HeaderMenu />
           <main>{children}</main>
